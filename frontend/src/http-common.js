0ebf4e41da1209140c35f9e5603d815dbcfcb98d
Zhi <neozhixuan@gmail.com>
2023-07-13 18:21:51 +0800

    Update http-common.js

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://referall-backend.vercel.app/api/v1/",
+  withCredentials: true,
+  credentials: "include",
   headers: {
     "Content-type": "application/json",
   },
