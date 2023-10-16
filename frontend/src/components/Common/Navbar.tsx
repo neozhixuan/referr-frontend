627dc340a77cc91d4358a1f5b4c80baa7afd7121
Zhi <neozhixuan@gmail.com>
2023-10-16 17:51:37 +0800

    Bug - Navbar gap when logged in

--- frontend/src/components/Common/Navbar.tsx
+++ frontend/src/components/Common/Navbar.tsx
@@ -65,7 +65,7 @@ const Navbar = ({ navHeight, Logout }: navType) => {
           referr.site
         </span>
       </a>
-      <div className="navbar-nav w-full d-flex flex-row gap-4">
+      <div className="navbar-nav w-full d-flex flex-row gap-2">
         <li className="nav-item">
           <a
             href="https://forms.gle/hG6zi9aRJrPLEb327"
